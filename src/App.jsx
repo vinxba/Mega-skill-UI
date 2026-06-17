30e25a400ffcbbc05e036c505c7f49fac41e7886
varuncareergize <varun.124150@gmail.com>
2026-06-17 07:12:26 +0530

    foooter_fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from "./components/Services";
